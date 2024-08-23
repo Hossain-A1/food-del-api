134c5d051fde04095cfc7f1bdb9f7f7649e2e9fe
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-08-23 09:29:53 +0300

    done

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -58,7 +58,7 @@ const verifyOrder = async (req, res) => {
   const { orderId, success } = req.body;
 
   try {
-    if (success == "true") {
+    if (success === "true") {
       await orderModel.findByIdAndUpdate(orderId, { payment: true });
       res.json({ success: true, message: "Paid" });
     } else {
@@ -84,4 +84,30 @@ const userOrders = async (req, res) => {
   }
 };
 
-export { placeOrder, verifyOrder, userOrders };
+// listing orders for admin panel
+
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// order status updating
+
+const updateSattus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+    res.json({ success: true, message: "Status updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateSattus };
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {placeOrder, userOrders, verifyOrder }from "../controllers/order.controller.js";
+import {listOrders, placeOrder, updateSattus, userOrders, verifyOrder }from "../controllers/order.controller.js";
 import { isAuthorized } from "../middlewares/auth.middleware.js";
 
 const orderRouter = express.Router();
@@ -7,5 +7,7 @@ const orderRouter = express.Router();
 orderRouter.post("/order", isAuthorized, placeOrder);
 orderRouter.post('/verify',verifyOrder)
 orderRouter.post('/getorders',isAuthorized,userOrders)
+orderRouter.get('/list/orders',listOrders)
+orderRouter.post('/order/status',updateSattus)
 
 export default orderRouter;
